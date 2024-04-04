1d0eb207aeda0dde3283476f2db4ef4744e924ac
fraserivison <fraserivison@hotmail.co.uk>
2024-04-04 17:35:22 +0000

    Add javascript code that will call upon the username throughout the site with the user-greeting class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
+// Splash Page
 document.getElementById("name-form").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent form submission
 
     let userName = document.getElementById("username").value;
 
-    // Store the username in localStorage for future use
+    // Store the username in localStorage for future use, MAYBE DELETE
     localStorage.setItem("username", userName);
 
     // Hide the splash content and show the main content
     document.getElementById("splash-content").style.display = "none";
     document.getElementById("main-content").style.display = "block";
 
-    // Display the username in the greeting
-    document.getElementById("user-greeting").textContent = userName;
+    // Display the username in the when called upon
+    let userGreetings = document.getElementsByClassName("user-greeting");
+    for (let i = 0; i < userGreetings.length; i++) {
+        userGreetings[i].textContent = userName;
+    }
+
 });

fe4bb3d8c62a276a3c2b3d3b6e558711bd335afd
fraserivison <fraserivison@hotmail.co.uk>
2024-04-11 19:52:33 +0000

    Make file size smaller as it is 8MB, now 135KB for main image on home.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,9 @@ document.addEventListener("DOMContentLoaded", function () {
             window.location.href = "home.html";
         });
     }
+    // Preload main image on home.html
+    const image = new Image();
+        image.src = "assets/images/astronaut.jpeg";
 
     // Retrieve the username from local storage
     let username = localStorage.getItem("username");
@@ -44,9 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
-// home.html
-const image = new Image();
-        image.src = "assets/images/astronaut.jpeg"; 
+// home.html 
 
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {

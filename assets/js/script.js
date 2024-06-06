b4e0393451a543e9db44a6b78a6b54021c3c0f78
fraserivison <fraserivison@hotmail.co.uk>
2024-06-06 14:45:53 +0000

    Move spaceQuiz to quiz.js and fix functionality so javacsript file is linked correctly to quiz.html and add event listener at the end of quiz.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,58 +2,6 @@
 
 // Main Quiz
 document.addEventListener("DOMContentLoaded", function () {
-    let spaceQuiz = [
-        {
-            question: "What is the approximate diameter of the observable universe?",
-            options: ["93 million light-years", "93 billion light-years", "93 trillion light-years", "930 billion light-years"],
-            answer: "93 billion light-years"
-        },
-        {
-            question: "What is the mass of the black hole at the center of our galaxy?",
-            options: ["1 million Suns", "4 billion Suns", "10 billion Suns", "100 billion Suns"],
-            answer: "4 billion Suns"
-        },
-        {
-            question: "Who was the first person to set foot on the lunar surface?",
-            options: ["Buzz Aldrin", "Neil Armstrong", "Alan Shepard", "John Glenn"],
-            answer: "Neil Armstrong"
-        },
-        {
-            question: "What was the name of the first living being sent into orbit around the Earth?",
-            options: ["Rover", "Laika", "Astro", "Luna"],
-            answer: "Laika"
-        },
-        {
-            question: "What milestone did Valentina Tereshkova achieve in space exploration?",
-            options: ["First woman to orbit Mars", "First woman in space", "First woman to walk on the Moon", "First woman to reach the International Space Station"],
-            answer: "First woman in space"
-        },
-        {
-            question: "What is the expected outcome of the collision between the Milky Way and the Andromeda galaxy?",
-            options: ["Formation of a new star", "Formation of a black hole", "Formation of a supernova", "Formation of an elliptical galaxy"],
-            answer: "Formation of an elliptical galaxy"
-        },
-        {
-            question: "What is the speed of light in miles per second?",
-            options: ["186,000", "669.6 million", "93 billion", "4 billion"],
-            answer: "186,000"
-        },
-        {
-            question: "How many times did Valentina Tereshkova orbit the Earth during her mission?",
-            options: ["24", "36", "48", "60"],
-            answer: "48"
-        },
-        {
-            question: "Who became the first Canadian to command the International Space Station (ISS)?",
-            options: ["Chris Hadfield", "Roberta Bondar", "Marc Garneau", "Julie Payette"],
-            answer: "Chris Hadfield"
-        },
-        {
-            question: "What percentage of the universe's gravity is believed to be due to dark matter?",
-            options: ["50%", "65%", "85%", "100%"],
-            answer: "85%"
-        }
-    ]
 // index.html
     let form = document.getElementById("name");
     if (form) {
